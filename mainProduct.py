e4f80c74a82f937dad408b841d17137e1901a28a
maria-lanecc <130002432+maria-lanecc@users.noreply.github.com>
2023-05-16 18:28:16 -0700

    Add files via upload

---
+++ mainProduct.py
@@ -0,0 +1,24 @@
+from productListTests import *
+
+def main():
+    testConstructor()
+    print()
+    testAppend()
+    print()
+    testIn()
+    print()
+    testFind()
+    print()
+    createList()
+    print()
+    testForLoop()
+    print()
+    testAdd()
+    print()
+    testPop()
+    print()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
